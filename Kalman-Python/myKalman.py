f29a2b46340741cf29b938b1efdf522f51f3b8af
LeoJhonSong_Desktop <LeoJhon.Song@outlook.com>
2018-12-08 23:37:44 +0800

    this version considers velocity as a element of state and is not that effective.

--- Kalman-Python/myKalman.py
+++ Kalman-Python/myKalman.py
@@ -1,10 +1,12 @@
 import numpy as np
-import pylab
+import matplotlib.pyplot as plt
 
 
-P = 5e-2
-Q = 1e-9
-R = 1e-4
+P = 7e-1
+Qa = 3e-3
+Ra = 1e-7
+Qv = 7.5e-2
+Rv = 5e-3
 
 
 class myKalman(object):
@@ -37,8 +39,8 @@ class myKalman(object):
     #       statePost   (xk)is recommended to use a measurement to initialize
     #                       statePost = statePre + gain * (z(k) - measureTrans * statePre)
     #                       statePost rely more on statePre when Q is small, vise versa
-    def __init__(self, stateDimen, measureDimen, controlDimen):  # controlDimen may not used, in this case input 0
-        self.start = False
+    # controlDimen may not used, in this case input 0
+    def __init__(self, stateDimen, measureDimen, controlDimen):
         self.stateDimen = stateDimen
         self.measureDimen = measureDimen
         if controlDimen:
@@ -59,10 +61,12 @@ class myKalman(object):
             self.measureTrans = np.mat(measureTrans)
 
     def setQ(self, Q):
-        self.prcssNsCov = np.full((self.stateDimen, self.stateDimen), Q)
+        Q = np.mat(Q)
+        self.prcssNsCov = Q.reshape(-1, 1) * Q
 
     def setR(self, R):
-        self.msrNsCov = np.full((self.stateDimen, self.stateDimen), R)
+        R = np.mat(R)
+        self.msrNsCov = R.reshape(-1, 1) * R
 
     def setB(self, controlTrans):
         if self.controlSwitch:
@@ -74,7 +78,8 @@ class myKalman(object):
         #       P'(k) = A * P(k-1) * A.T + Q
         self.control = np.mat(control)
         self.statePre = self.transition * self.statePost + self.controlTrans * self.control
-        self.priEstErrCov = self.transition * self.postEstErrCov * self.transition.T + self.prcssNsCov
+        self.priEstErrCov = self.transition * \
+            self.postEstErrCov * self.transition.T + self.prcssNsCov
 
     def correct(self, measure):
         #   correct
@@ -84,51 +89,71 @@ class myKalman(object):
         #       x_hat(k) = x_hat'(k) + K(k) * (z(k) - H * x_hat'(k))
         #       P(k) = (I - K(k) * H) * P'(k)
         self.measure = np.mat(measure)
-        self.msrResCov = self.measureTrans * self.priEstErrCov * self.measureTrans.T + self.msrNsCov
+        self.msrResCov = self.measureTrans * self.priEstErrCov * \
+            self.measureTrans.T + self.msrNsCov
         self.gain = self.priEstErrCov * self.measureTrans.T * self.msrResCov
-        self.statePost = self.statePre + self.gain * (self.measure - self.measureTrans * self.statePre)
-        self.postEstErrCov = (np.identity(self.stateDimen) - self.gain * self.measureTrans) * self.priEstErrCov
+        self.statePost = self.statePre + self.gain * \
+            (self.measure - self.measureTrans * self.statePre)
+        self.postEstErrCov = (np.identity(
+            self.stateDimen) - self.gain * self.measureTrans) * self.priEstErrCov
 
     def new(self, measure, control):  # control may not needed
         # initialing the state with measurement allows the signal converge more easily
         # initialing the post estimate error covariance with an identity matrix because the initial value does not matter that much
-        if not self.start:
-            self.statePost = measure
-            self.postEstErrCov = np.full((self.stateDimen, self.stateDimen), P)
-            self.start = True
         self.predict(control)
         self.correct(measure)
 
 
-# period = 5ms
-t = 0.0005
+# initial the filter
 # [[angle], [velocity]]
 motor = myKalman(2, 2, 1)
 # the transition matrix and transform matrix for control vector is simply based on Newton's law
+# period = 5ms
+t = 0.0005
 motor.setAH([[1, t], [0, 1]], 0)
 motor.setB([[t * t / 2], [t]])
-motor.setQ(Q)
-motor.setR(R)
+motor.setQ([Qa, Qv])
+motor.setR([Ra, Rv])
+motor.postEstErrCov = np.full((motor.stateDimen, motor.stateDimen), P)
+
 velocity = 0
 velocity_last = 0
+
 x = []
 y = []
 # adjust Q, R with the test input
-input = np.loadtxt('../test_data/with_command.txt')  # angle, control, velocity
+input = np.loadtxt('./test_data/without_command.txt')  # angle, control, velocity
+
+motor.statePost = np.mat([[input[0, 0]], [input[0, 2]*2*np.pi/60/t]])
+
 for column in input:
     angleIn = column[0]
     controlIn = column[1]
     velocityIn = column[2]
+
     # then transfer the units
     angle = angleIn
-    control = (controlIn * 2 * np.pi / 60 - velocity) / t  # indicate acceleration by increacesment
+    control = (controlIn * 2 * np.pi / 60 - velocity) / \
+        t  # indicate acceleration by increacesment
     velocity = velocityIn * 2 * np.pi / 60
+
     motor.new(([angle], [velocity]), control)
-    x.append(motor.statePost[1, 0])
-    y.append(motor.postEstErrCov)
-    # 然后plot之类的
-pylab.figure(1)
-pylab.plot((input[:, 2] * 2 * np.pi / 60), color='r', label='measurement')
-pylab.plot(x, color='g', label='filter')
-pylab.show()
+
+    x.append(motor.statePost[0, 0])
+    y.append(motor.statePost[1, 0])
+
+# 然后plot之类的
+# 定义 x 变量的范围, 数量
+time = np.linspace(0, input.shape[0], input.shape[0])
+
+plt.subplot(211)
+plt.plot(time, x, color='green')
+plt.plot(time, input[:, 0], color='red', linestyle='--')
+
+plt.subplot(212)
+plt.plot(time, y, color='green')
+plt.plot(time, (input[:, 2]*2*np.pi/60), color='red', linestyle='--')
+
+plt.show()
+
 i = 1
